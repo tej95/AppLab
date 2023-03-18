205c2b176b281bb17216ba7ff911cdc9529475bd
tej95 <tejash.agrawal@learner.manipal.edu>
2023-03-19 04:08:03 +0530

    final commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Header from '../components/header'
-import Lottie from "lottie-react";
-import laptopAnimation from '../components/138767-laptop-animatiion.json'
 
 const Contact = () => {
   return (
   <>
 
 
-  {/* <Header color="navColor"></Header> */}
-   <Lottie animationData={laptopAnimation} />
+  <Header color="navColor"></Header>
+   <div>contact</div>
   
   
   
